6d2728228da23462b3ac2b581a3022a536368546
Harsh Sharma <harshsharmaktm03@gmail.com>
2024-01-12 20:42:59 -0800

    product page initialComponent done 12jan 2k24

--- src/ProductComponents/InitialVideo.jsx
+++ src/ProductComponents/InitialVideo.jsx
@@ -11,16 +11,35 @@ function InitialVideo() {
         height: useBreakpointValue({ base: "70vh", md: "100vh", lg: "100vh" }),
         objectFit: 'cover',
     }
+    const videoStyles2 = {
+        width: '100%',
+        height: useBreakpointValue({ base: "80vh", md: "100vh", lg: "100vh" }),
+        objectFit: 'cover',
+    }
 
     const TextStyle = {
         color: "#fff",
-        fontSize: ""
+        fontSize: "",
+        position:"absolute",
+        textAlign:"center",
+        top:"0",
+        left:"0",
+        background:"#000",
+        height:"100%",
+        width:"100%",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        fontSize:useBreakpointValue({ base: "80px", md: "100px", lg: "140px" }),
+        fontWeight:"500",
+        mixBlendMode:"multiply",
+        flexDirection:"column"
     }
 
     return (
         <>
 
-            <Box w="100%" h="240vh" bg="#000" border="2px solid lime" position="relative">
+            <Box w="100%" h={{base:"",md:"230vh"}} bg="#000"  position="relative">
 
                 <video
                     style={videoStyles}
@@ -34,24 +53,35 @@ function InitialVideo() {
                 </video>
 
 
-                {/* <video
-                    style={videoStyles}
-                    loop
+                <Box w="100%" h={{base:"130vh",md:"130vh"}} position={"relative"}>
+                    <Box w="100%" h="auto" height={"30vh"}></Box>
 
-                    autoPlay={true} // Set to true if you want videos to autoplay
-                    muted // Adding this video to start muted
-                >
-                    <source src={"https://dji-official-fe.djicdn.com/reactor/assets/_next/static/videos/f8221748-07ec-4bc6-a050-ad1cda891694.mp4"} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video> */}
+                    <video
+                        style={videoStyles2}
+                        loop
+
+                        autoPlay={true} // Set to true if you want videos to autoplay
+                        muted // Adding this video to start muted
+                    >
+                        <source src={"https://dji-official-fe.djicdn.com/reactor/assets/_next/static/videos/f8221748-07ec-4bc6-a050-ad1cda891694.mp4"} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <Box style={TextStyle}>
+
+                        <Text mt={{base:"",md:""}} mb={{base:"-10%",md:"-5%"}}>See the</Text>
+                        <Text mb={{base:"-10%",md:"-5%"}} >unknown</Text>
+                        <Text mb={{base:"-10%",md:"-5%"}} >world</Text>
+                        <Text mb={{base:"-10%",md:"-5%"}} >in your eyes</Text>
+                          
+                    </Box>
 
 
-                <Box w="100%" h="auto" height={"40vh"}></Box>
+                </Box>
 
-                <Text textAlign={"center"} color={"#fff"} fontSize={{ base: "80px", md: "140px" }} mb={{ base: "-10%", md: "-6%" }}>See the</Text>
-                <Text textAlign={"center"} color={"#fff"} fontSize={{ base: "80px", md: "140px" }} mb={{ base: "-10%", md: "-6%" }}>unknown</Text>
-                <Text textAlign={"center"} color={"#fff"} fontSize={{ base: "80px", md: "140px" }} mb={{ base: "-10%", md: "-6%" }}>world</Text>
-                <Text textAlign={"center"} color={"#fff"} fontSize={{ base: "80px", md: "140px" }} mb={{ base: "-10%", md: "-6%" }}>in your eyes</Text>
+
+
+
+
 
 
 
