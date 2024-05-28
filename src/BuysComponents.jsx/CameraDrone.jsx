493832972623e54507a0ea48d89d857a57b146a1
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2024-05-28 12:24:21 +0530

    BUG found in aerial and immersive tabs

--- src/BuysComponents.jsx/CameraDrone.jsx
+++ src/BuysComponents.jsx/CameraDrone.jsx
@@ -1,10 +1,34 @@
-import React, { useState, useContext } from 'react';
-import { Text, Box, Divider, Image, Button, useBreakpointValue, Grid, Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs, GridItem } from "@chakra-ui/react";
+import React, { useState, useContext, useEffect } from 'react';
+import { Text, Box, Divider, Image, Button, useBreakpointValue, Grid, Tab, TabIndicator, TabList, TabPanel, TabPanels, Tabs, GridItem, } from "@chakra-ui/react";
 import Navbar from '../Components/Navbar.jsx';
 import Footer from "../Components/Footer.jsx"
 import FourImg from "../HomeComponents/FourImg.jsx"
 import { AuthContext } from '../Contexts/AuthContextProvider.jsx';
+import axios from "axios";
 function CameraDrone() {
+  const [drones, setDrones] = useState([]);
+  const [immersive, setImmersive] = useState([]);
+  const fetchDroneData = async () => {
+    try {
+
+      const response1 = await axios.get('http://localhost:4000/camera_drones/get/aerial');
+      setDrones(response1.data.get_data[0].drones)
+      // console.log(drones)
+
+      const response2 = await axios.get('http://localhost:4000/camera_drones/get/immersive');
+      setImmersive(response2.data.get_data[0].drones)
+      console.log(immersive)
+
+    } catch (error) {
+      // Handle error here
+      console.error('Error creating user:', error);
+    }
+  }
+  useEffect(() => {
+    fetchDroneData()
+  }, [])
+
+
   const { isAuth, token } = useContext(AuthContext)
   console.log(isAuth, token)
   // Array of images
@@ -34,7 +58,7 @@ function CameraDrone() {
 
 
 
-      <Box w="100%" h="100vh" bg="#fff">
+      <Box w="100%" h="auto" bg="#fff">
         <Box w="100%" h={{ base: "21.5vh", md: "20vh" }} bg="#F5F5F5">
 
           <Box w={{ base: "100%", md: "80%" }} m="auto" h="100%" bg="#" style={{ overflow: "hidden" }}>
@@ -94,7 +118,7 @@ function CameraDrone() {
         {/* camera drones heading box ends here-------------->>>>>>>>>> */}
 
         {/* Dji Avata 2 hero part starting from here------------->>>>>>>>>>>> */}
-        <Box className="hero_wrapper" w='100%' h={{ base: "80vh", md: "90vh" }} display={"flex"} justifyContent={"center"} alignContent={"center"} alignItems={"center"}
+        <Box  w='100%' h={{ base: "80vh", md: "90vh" }} display={"flex"} justifyContent={"center"} alignContent={"center"} alignItems={"center"}
         >
           <Box
             position={"relative"}
@@ -133,7 +157,7 @@ function CameraDrone() {
 
         {/* Aerial Imaging || Immersive Flight  system building starts here----------------->>>>>>>>>>>>>>>*/}
 
-        <Box w={"100%"} h={{ base: "auto", md: "auto" }} flexDirection={"column"} justifyContent={"center"} display={"flex"} alignContent={"center"} alignItems={"center"}>
+        <Box border={"5px solid coral"} w={"100%"} h={{ base: "auto", md: "auto" }} flexDirection={"column"} justifyContent={"center"} display={"flex"} alignContent={"center"} alignItems={"center"}>
           <Box w='80%' h={{ base: "20vh", md: "30vh" }} justifyContent={"center"} display={"flex"} alignContent={"center"} alignItems={"center"}>
             <Text textAlign={"center"} fontSize={{ base: "30px", md: "5xl" }} fontWeight={"500"}>
               Which camera drone is right for you?
@@ -162,15 +186,16 @@ function CameraDrone() {
             </Box>
 
             <TabPanels>
-              <TabPanel w={{ base: "350px", md: "900px" }} h="auto">
+              <TabPanel w={{ base: "350px", md: "900px" }} h="150vh">
 
                 {/* Aerial Imaging container here----------------------->>>>>>>>>>>> */}
                 <Box
                   w={{ base: "100%", md: "100%" }} border="1px solid red" h="100%"
                 >
-                  <Grid templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)' }} gap={3} >
+                  <Grid  templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)' }} gap={3} >
                     {/* mapping all the Aerial Imagin data from backend here------>>>> */}
-                    <Box w="100%" h="auto" bg="fff">
+                    {drones.map((drone, index) => (
+                    <Box w="100%" h="auto" bg="fff" key={index} className={`grid-item column-${index + 1}`}>
 
 
 
@@ -178,7 +203,7 @@ function CameraDrone() {
 
                       {/* product image */}
                       <Box w={{ base: "90%", md: "90%" }} m="auto" h={{ base: "16vh", md: "30vh" }} >
-                        <Image src="https://dji-official-fe.djicdn.com/cms/uploads/ae5d8b9987be8d5ecdeb5d502a1e887c.png" alt="" />
+                        <Image src={drone.img_url} alt={"drone.png"} />
                       </Box>
                       {/* product image */}
 
@@ -193,7 +218,7 @@ function CameraDrone() {
 
 
                       {/* pro name */}
-                      <Text textAlign={"center"} fontWeight={"350"} fontSize={{ base: "16px", md: "25px" }} color={"#3a3738"}>DJI Mavic 3 Pro</Text>
+                      <Text textAlign={"center"} fontWeight={"350"} fontSize={{ base: "16px", md: "25px" }} color={"#3a3738"}>{drone.pro_title}</Text>
                       {/* pro name */}
 
 
@@ -248,7 +273,7 @@ function CameraDrone() {
                         w="100%" h={{ base: "120px", md: "210px" }}
                       // border={"1px solid cyan"}
                       >
-                        <Text mt="10px" textAlign={"center"} fontWeight={"500"} fontSize={{ base: "24px", md: "42px" }} color={"#000"}>958 g</Text>
+                        <Text mt="10px" textAlign={"center"} fontWeight={"500"} fontSize={{ base: "24px", md: "42px" }} color={"#000"}>{drone.pro_weight}</Text>
                         <Text mt="-3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>Takeoff Weight</Text>
 
                         <Box
@@ -262,7 +287,7 @@ function CameraDrone() {
                         >
                           <Image w={{ base: "20%", md: "18%" }} h={"auto"} src="https://dji-official-fe.djicdn.com/dps/f9127d27145b69a9c196c225c91e4e32.svg" alt="svg" />
                           <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>
-                            43 mins Max Flight Time
+                            {drone.flight_time}
                           </Text>
                         </Box>
                       </Box>
@@ -288,9 +313,7 @@ function CameraDrone() {
                       // border="1px solid red"
                       >
                         <Image w={{ base: "20%", md: "18%" }} h="auto" src="https://dji-official-fe.djicdn.com/dps/cd0846ffff48ba7425dcb2a71b58f28c.svg" alt="svg" />
-                        <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>4/3 CMOS Hasselblad Camera
-                          1/1.3-inch CMOS Medium Tele Camera
-                          1/2-inch CMOS Tele Camera
+                        <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>{drone.camera_info}
                         </Text>
                       </Box>
                       {/* camera resolution */}
@@ -301,7 +324,7 @@ function CameraDrone() {
 
 
                       {/* photo resolution */}
-                      <Text mt="10px" textAlign={"center"} fontWeight={"500"} fontSize={{ base: "24px", md: "42px" }} color={"#000"}>48 MP</Text>
+                      <Text mt="10px" textAlign={"center"} fontWeight={"500"} fontSize={{ base: "24px", md: "42px" }} color={"#000"}>{drone.camera_photo_reso}</Text>
                       <Text mt="-3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>Max Photo Resolution</Text>
                       {/* photo resolution */}
 
@@ -317,13 +340,11 @@ function CameraDrone() {
                         alignItems={"center"}
                         flexDirection={"column"}
                         mt="10px" w="100%" h="auto"
-                        //  border="1px solid red"
-                         >
+                      //  border="1px solid red"
+                      >
                         <Image w={{ base: "20%", md: "18%" }} h="auto" src="https://dji-official-fe.djicdn.com/dps/3d0d77b212222ab4f20219cc95ca6d20.svg" alt="svg" />
-                        <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>DJI O3+ HD Digital Video Transmission
-
-                          Up to 15 km Transmission Distance
-                          1080p/60fps Live Feed
+                        <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>
+                          {drone.tx_range}
                         </Text>
                       </Box>
                       {/* transmiter range */}
@@ -333,22 +354,117 @@ function CameraDrone() {
 
 
                     </Box>
+
+                    ))}
                   </Grid>
 
                 </Box>
               </TabPanel>
-              <TabPanel w={{ base: "350px", md: "900px" }} border="1px solid blue" h="150vh">
+              <TabPanel w={{ base: "350px", md: "900px" }} border="1px solid blue" h="auto">
                 <Box
                   w={{ base: "100%", md: "100%" }} border="1px solid blue" h="100%"
                 >
                   <Grid templateColumns={{ base: 'repeat(2, 1fr)', md: 'repeat(3, 1fr)' }} gap={3} >
                     {/* mapping all the Aerial Imagin data from backend here------>>>> */}
-                    <Box w="100%" h="120vh" bg="#eee">
+                    {immersive.map((drone, index) => (
+                    <Box w="100%" h="auto" key={index} bg="fff">
 
 
-                      <Box w={{ base: "90%", md: "90%" }} m="auto" h={{ base: "16vh", md: "30vh" }} border={"1px solid lime"}>
-                        <Image src="https://dji-official-fe.djicdn.com/cms/uploads/ae5d8b9987be8d5ecdeb5d502a1e887c.png" alt="" />
+
+
+
+                      {/* product image */}
+                      <Box w={{ base: "90%", md: "90%" }} m="auto" h={{ base: "16vh", md: "30vh" }} >
+                        <Image src={drone.img_url} alt="" />
                       </Box>
+                      {/* product image */}
+
+
+
+
+
+                      <Text textAlign={"center"} fontSize={{ base: "10px", md: "12px" }} color={"#d0021b"}>Flagship</Text>
+
+
+
+
+
+                      {/* pro name */}
+                      <Text textAlign={"center"} fontWeight={"350"} fontSize={{ base: "16px", md: "25px" }} color={"#3a3738"}>{drone.pro_title}</Text>
+                      {/* pro name */}
+
+
+
+
+                      {/* where to buy  */}
+                      {/* learn more */}
+                      <Box
+                        display={"flex"}
+                        justifyContent={"space-evenly"}
+                        alignContent={"center"}
+                        alignItems={"center"}
+                        w="100%" h="100px"
+                        flexDirection={"column"}
+                      // border="1px solid red"
+                      >
+
+                        <Box
+                          display={"flex"}
+                          justifyContent={"center"}
+                          alignContent={"center"}
+                          alignItems={"center"}
+                          _hover={{ cursor: "pointer", bg: "#4897ef" }}
+                          w={{ base: "50%", md: "40%" }}
+                          height={{ base: "30px", md: "30px" }}
+                          borderTopRightRadius={"17px"}
+                          borderTopLeftRadius={"17px"}
+                          borderBottomRightRadius={"17px"}
+                          borderBottomLeftRadius={"17px"}
+                          bg="#2484ef">
+
+                          <Text textAlign={"center"} fontWeight={"400"} fontSize={{ base: "10px", md: "15px" }} color={"#fff"}>Where to buy</Text>
+                        </Box>
+                        <Box w="100%" h="auto" >
+                          <Text _hover={{ cursor: "pointer", textDecoration: "underline" }} textAlign={"center"} fontWeight={"500"} fontSize={{ base: "10px", md: "12px" }} color={"#4897ef"}>Learn more {">"}</Text>
+                          <Divider />
+                        </Box>
+                      </Box>
+                      {/* where to buy  */}
+                      {/* learn more */}
+
+
+
+
+
+
+
+
+                      {/*pro  weight */}
+                      {/* pro flight time */}
+                      <Box
+                        w="100%" h={{ base: "120px", md: "210px" }}
+                      // border={"1px solid cyan"}
+                      >
+                        <Text mt="10px" textAlign={"center"} fontWeight={"500"} fontSize={{ base: "24px", md: "42px" }} color={"#000"}>{drone.pro_weight}</Text>
+                        <Text mt="-3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>Takeoff Weight</Text>
+
+                        <Box
+                          display={"flex"}
+                          justifyContent={"center"}
+                          alignContent={"center"}
+                          alignItems={"center"}
+                          flexDirection={"column"}
+                          mt="10px" w="100%" h="auto"
+                        // border="1px solid purple"
+                        >
+                          <Image w={{ base: "20%", md: "18%" }} h={"auto"} src="https://dji-official-fe.djicdn.com/dps/f9127d27145b69a9c196c225c91e4e32.svg" alt="svg" />
+                          <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>
+                           {drone.flight_time}
+                          </Text>
+                        </Box>
+                      </Box>
+                      {/*pro  weight */}
+                      {/* pro flight time */}
 
 
 
@@ -358,13 +474,59 @@ function CameraDrone() {
 
 
 
+                      {/* camera resolution */}
+                      <Box
+                        display={"flex"}
+                        justifyContent={"center"}
+                        alignContent={"center"}
+                        alignItems={"center"}
+                        flexDirection={"column"}
+                        mt="10px" w="100%" h="auto"
+                      // border="1px solid red"
+                      >
+                        <Image w={{ base: "20%", md: "18%" }} h="auto" src="https://dji-official-fe.djicdn.com/dps/cd0846ffff48ba7425dcb2a71b58f28c.svg" alt="svg" />
+                        <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>
+                          {drone.camera_info}
+                        </Text>
+                      </Box>
+                      {/* camera resolution */}
 
+
+
+
+
+
+                      {/* camera angle*/}
+                      <Text mt="10px" textAlign={"center"} fontWeight={"500"} fontSize={{ base: "24px", md: "42px" }} color={"#000"}>{drone.camera_angle}</Text>
+                      <Text mt="-3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>Ultra-Wide FOV</Text>
+                      {/* photo resolution */}
+
+
+
+
+
+                      {/* transmiter range */}
+                      <Box
+                        display={"flex"}
+                        justifyContent={"center"}
+                        alignContent={"center"}
+                        alignItems={"center"}
+                        flexDirection={"column"}
+                        mt="10px" w="100%" h="auto"
+                      //  border="1px solid red"
+                      >
+                        <Text mt="3px" textAlign={"center"} fontWeight={"300"} fontSize={{ base: "11px", md: "16px" }} color={"#000"}>
+                          {drone.discription}
+                        </Text>
+                      </Box>
+                      {/* transmiter range */}
 
 
 
 
 
                     </Box>
+                    ))}
                   </Grid>
                 </Box>
 
@@ -377,6 +539,16 @@ function CameraDrone() {
 
 
 
+        {immersive.map((drone, index) => (
+          <>
+
+          <img  src={drone.img_url}  alt={index} />
+          </>
+        ))}
+        
+
+
+        
 
 
 
@@ -399,19 +571,13 @@ function CameraDrone() {
 
 
 
+      {/* Aerial Imaging || Immersive Flight  system building starts here----------------->>>>>>>>>>>>>>>*/}
 
 
+      <Footer />
 
 
-
-
-        {/* Aerial Imaging || Immersive Flight  system building starts here----------------->>>>>>>>>>>>>>>*/}
-
-
-        <Footer />
-
-
-      </Box >
+    </Box >
 
 
 
