275af509ff0ec450178cca4ee26e4264031df341
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2024-07-30 12:14:18 +0800

    Mobile nav fixes 2.0

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -224,7 +224,7 @@ const Navbar = () => {
               </Text>
               <FiShoppingCart display={{ base: isOpen ? 'none' : 'none', md: 'block' }} size={"20px"} />
             </Center>
-            <Text onClick={handleCart} display={{ base: isOpen ? 'block' : 'none', md: 'none' }} _hover={{ cursor: "pointer" }} fontWeight={{ base: "500", md: "400", lg: "400" }} fontSize={{ base: "16px", md: "14.5px" }} color="#4d4d4d" ml={{ base: "0px", md: "10px" }} mr={{ base: "0px", md: "10px" }} mt={{ base: "15px", md: "0px" }} mb={{ base: "15px", md: "0px" }} >
+            <Text onClick={handleSignup} display={{ base: isOpen ? 'block' : 'none', md: 'none' }} _hover={{ cursor: "pointer" }} fontWeight={{ base: "500", md: "400", lg: "400" }} fontSize={{ base: "16px", md: "14.5px" }} color="#4d4d4d" ml={{ base: "0px", md: "10px" }} mr={{ base: "0px", md: "10px" }} mt={{ base: "15px", md: "0px" }} mb={{ base: "15px", md: "0px" }} >
                 Signup
               </Text>
               <Text onClick={isAuth !== null ? handleLogout : handleLogin} display={{ base: isOpen ? 'block' : 'none', md: 'none' }} _hover={{ cursor: "pointer" }} fontWeight={{ base: "500", md: "400", lg: "400" }} fontSize={{ base: "16px", md: "14.5px" }} color="#4d4d4d" ml={{ base: "0px", md: "10px" }} mr={{ base: "0px", md: "10px" }} mt={{ base: "15px", md: "0px" }} mb={{ base: "15px", md: "0px" }} >
